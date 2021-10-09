f81cc2861c1b05ec717eae4682f857f1d76b1f95
ChriZoizo <christeufeur83@gmail.com>
2021-10-09 02:49:08 +0200

    code(product.js, cart.js) function for take infos of products (qty, color) and save into localStorage. FAILED. NEED TO FIX 09/10/2021 @02:48

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,24 +1,30 @@
-function testing(arr) {
-    let objectToSave = JSON.stringify(arr)
-    localStorage.setItem("productInCart", objectToSave)
+function testing() {
     let display = localStorage.getItem("productInCart")
     let displayed = JSON.parse(display)
     console.log(displayed)
+    return displayed
   }
 
   let display = localStorage.getItem("productInCart")
   let displayed = JSON.parse(display)
+  
+loop(displayed)
   console.log(displayed)
 
 
   function loop(arr) {
       for (let i = 0; i < arr.length; i++) {
  populateCardWithProducts(arr[i])
+ console.log("AAAAAAAAAAAAAAAAAAAAAAAAA")
 
 }
   }
 
+
+
+
   function populateCardWithProducts(object){
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAA")
       const articleContainer = document.getElementById("cart__items")
       let newArticleItemLeft = document.createElement("article")
       newArticleItemLeft.setAttribute("class", "cart__item")
@@ -30,14 +36,22 @@ function testing(arr) {
       newImg.setAttribute("alt", object.altTxt)
 let newArticleItemContent = document.createElement("div")
 newArticleItemContent.setAttribute("classe", "cart__item__content")
-let newArticleItemContentTitlePrice = document.createElement("div")
-newArticleItemContentTitlePrice.setAttribute("class", "cart__item__content__titlePrice")
-let newArticleItemContentTitle = document.createElement("h2")
-newArticleItemContentTitle.innerText = object.name
-let newArticleItemContentPrice = document.createElement("p")
-newArticleItemContentPrice.innexText = object.price + " €"
-let newArticleItemContentSettings = document.createElement('div')
-newArticleItemContentSettings.setAttribute("class", "cart__item__content__settings" )
+let newArticleItemPriceContainer = document.createElement("div")
+newArticleItemPriceContainer.setAttribute("class", "cart__item__content__titlePrice")
+let newArticleItemTitle = document.createElement("h2")
+newArticleItemTitle.innerText = object.name
+let newArticleItemPrice = document.createElement("p")
+newArticleItemPrice.innexText = object.price + " €"
+let newArticleItemSettings = document.createElement('div')
+newArticleItemSettings.setAttribute("class", "cart__item__content__settings" )
+let newArticleQuantity = document.createElement("div")
+newArticleQuantity.setAttribute("class", "cart__item__content__settings__quantity")
+let articleQuantity = document.createElement("p")
+articleQuantity.innerText = "90"
 
+articleContainer.appendChild(newArticleItemLeft)
+console.log("AAAAAAAAAAAAAAAAAAAAAAAAA")
+newArticleItemLeft.appendChild(newDivForImg)
+newDivForImg.appendChild(newImg)
       
   }
--- front/js/product.js
+++ front/js/product.js
@@ -1,13 +1,28 @@
-const apiURL = "http://localhost:3000/api/products";
-const currentUrlInString = document.location.href
-const currentURL = new URL(currentUrlInString)
-const id = currentURL.searchParams.get("id")
-const productsList = getProductsFromApi(apiURL)
-console.log(id)
+// JEN SUIS AU MOMENT OU JE VEUX ENVOYER LES INFO AU LOCALSTORAGE? MAIS DANS 
+::FUNCTION EN QUESTION? LES PUSH N4ONT PAS L4AI R DE FONCTONNERCAR ILE NE RENVOIENT RIEN  DUT TouchListJE NE SAIS PAS COMMENT M4Y PREBNDRE 
 
-// Fonction permettant le fetch de l'api  et est censé retourner un array MAIS NE LE FAIT PAS !!!
-function getProductsFromApi(url) {
+
+
+
+const apiURL = "http://localhost:3000/api/products";
+
+//Recuperation de l'url actuelle sous forme de string
+const currentUrlInString = document.location.href
+// Création d'une nouvelle URL
+const currentURL = new URL(currentUrlInString)
+// recuperation de l'ID dans les params de la nouvelle URL
+const id = currentURL.searchParams.get("id")
+
+productArray = []
+
+const productToAdd = []
+ getProductFromApi(apiURL)
+
+
+// Fonction permettant les données de l'API
+async function getProductFromApi(url) {
   fetch(url)
+  // Jerecupere les données compléte de l'API
     .then(function (res) {
       if (res.ok) {
           console.log("fetch step 1")
@@ -15,55 +30,42 @@ function getProductsFromApi(url) {
         return res.json();
       }
     })
+    // Si elle sont ok, je selectionne le produit concerné via son ID
     .then(function (value) {
-      console.log(value);
-      console.log("values a the top of this is for step 2 fetching");
-      listOfProducts = value
-      console.log(listOfProducts)
-      displayOneProduct(listOfProducts.find(el => el._id == id))
-      let product = value
-
-      return value;
+       productArray.push(value.find(el => el._id == id)) 
+       productArray = productArray[0]
+      console.log(value.find(el => el._id == id))
+      console.log("TOP IS RETURN FUNCTION")
+/*       console.log(productSelected) */
+      console.log(productArray)
+      displayOneProduct(productArray)
+      return value.find(el => el._id == id);
     })
     .catch(function (err) {
         console.log("ERROR WITH API FETCHING. LOOK BOTTOM")
       console.log(err);
+      return err
     });
 }
 
-
-/* function getOneProduct(arr) {
-return arr.find(product => product._id === id)
-} */
-
-// Fonction pour recuperer et afficher la page d'un produit
-
+// Fonction pour recuperer et afficher les infos du produits concerné
 function displayOneProduct(object) {
-      // recuperation du conteneur pour l'image, creation de l'element
+  console.log("DISPLAYING" + object)
     const imageContainer = document.getElementsByClassName("item__img")
     let productImg = document.createElement("img")
-      // ajout des attributs et ajout dans le code HTML
+    console.log("Take IMAGE URL")
     productImg.setAttribute('src', object.imageUrl)
     productImg.setAttribute('alt', object.altTxt)
     imageContainer[0].appendChild(productImg)
-      // Recuperation de l'element conteneur pour le nom du produit
     const h1Container = document.getElementById("title")
-      // Injection dans le code HTML
     h1Container.innerText = object.name
-      // Recuperation de l'element conteneur pour le prix du produit
     const priceContainer = document.getElementById("price")
-      // injection dans le code HTML
     priceContainer.innerText = object.price
-      // Recuperation de l'element conteneur pour la descritpion
     const descriptionContainer = document.getElementById("description")
-      // Injection dans le code HTML
     descriptionContainer.innerText = object.description
-      // Recuperation du selecteur conteneur
-    const colorSelector = document.getElementById("colors") //A mettre dans la boucle ?
-    
+    const colorSelector = document.getElementById("colors") 
+    // Boucle sur l'array contenant les couleurs pour afficher
     let colorArray = object.colors
-
-
     for (i = 0; i < (object.colors.length); i ++){
         let thisColor = object.colors[i]
         let colorOption = document.createElement('option')
@@ -71,17 +73,41 @@ function displayOneProduct(object) {
         colorOption.innerText = thisColor
         colorSelector.appendChild(colorOption)
     }
-    testing(object)
 
+
+    /* testing(object) */
+    console.log(productArray)
 }
 
 
-function testing(arr) {
-  let objectToSave = JSON.stringify(arr)
-  localStorage.setItem("productInCart", objectToSave)
-  let display = localStorage.getItem("productInCart")
-  let displayed = JSON.parse(display)
-  console.log(displayed)
+function addToCart() {
+  let colorSelector = document.getElementById("colors")
+  let quantityForm = document.getElementById("quantity").value
+  let selectedColor = colorSelector.options[colorSelector.selectedIndex].value
+  if (selectedColor == "" && quantityForm == 0){
+    alert("Veuillez choisir une couleur et definir une quantitée")
+  }
+  else if (quantityForm == 0) {
+    alert("Veuillez Choisir une quantitée. Car sinon nous vous enverrons zero cannapé ^^")
+  }
+  else if (selectedColor == ""){
+    alert("Veuiller choisir une couleur")
+  }
+  else {
+    productToAdd["_id"]= productArray._id
+    productToAdd["color"] = selectedColor
+    productToAdd["quantity"] = quantityForm
+    console.log(productToAdd)
+
+
+    let objectToSave = new Array(productToAdd)
+    localStorage.setItem("productInCart", JSON.stringify(productArray._id + productArray.colors.find(el => el == selectedColor))
+    let display = localStorage.getItem("productInCart")
+    let displayed = JSON.stringify(display)
+    console.log(displayed)
+  }
+
+
 }
 
 
@@ -89,8 +115,16 @@ function testing(arr) {
 let addToCartButton = document.getElementById('addToCart')
 addToCartButton.addEventListener('click', addToCart)
 
-function addToCart(){
-  console.log("added to cart")
 
+async function addToCarte(){
+
+  console.log("adding to cart BU ALSO TESTINNNNNNNGGGGGG")
+  console.log(await getProductFromApi(apiURL))
+  const product = await getProductFromApi(apiURL)
+  console.log("MARQUER 1")
+  const detailOfProduct = await displayOneProduct(product)
+  console.log("MARQUER 2")
+  console.log(product)
+  console.log(productsList)
 }
 
